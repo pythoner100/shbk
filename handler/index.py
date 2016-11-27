45fa39a05ff9cb191edb33e6ca87f13d1d6f3030
xp1213 <3257662336@qq.com>
2016-11-27 05:47:36 -0800

    peizhi html

--- handler/index.py
+++ handler/index.py
@@ -2,8 +2,10 @@ import tornado.web
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write("Hello, world")
+        #self.write("Hello, world")
+        self.render('index.html')
 
 class HotHandler(tornado.web.RequestHandler):
     def get(self):
-        self.write('hot handler ')
+        #self.write('hot handler ')
+        self.render('hot.html')
--- shbk.py
+++ shbk.py
@@ -1,12 +1,17 @@
 import tornado.ioloop
 import tornado.web
 from handler.index import MainHandler, HotHandler
+import os
 
 def make_app():
+    basedir = os.path.dirname(__file__)
+    settings = {
+        'template_path': os.path.join(basedir, 'template')
+    }
     return tornado.web.Application([
         (r"/", MainHandler),
         (r"/hot", HotHandler)
-    ])
+    ], **settings)
 
 if __name__ == "__main__":
     app = make_app()
