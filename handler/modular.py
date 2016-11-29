adeb744903ae0b1f18473a5ee62b55c39ea4258d
xp1213 <3257662336@qq.com>
2016-11-29 03:38:41 -0800

    add word.html

--- handler/modular.py
+++ handler/modular.py
@@ -1,19 +1,26 @@
 import tornado.web
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         #self.write("Hello, world")
         self.render('index.html')
-
+          
 class HotHandler(tornado.web.RequestHandler):
     def get(self):
-        #self.write('hot handler ')
-        self.render('hot.html')
+        #self.write('hot handler ') 
+        self.render('hot.html') 
 
 class HourHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("hour.html")
-
+    
 class ChartHandler(tornado.web.RequestHandler):
     def get(self):
         self.render("chart.html")
+
+class WordHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.render("word.html")
+
+
